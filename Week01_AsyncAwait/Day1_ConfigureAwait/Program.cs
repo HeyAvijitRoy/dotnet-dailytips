965a4d928989df120859f0545f88e6935c40ed7f
Avijit Roy <mail@avijitroy.com>
2025-06-19 19:11:15 -0400

    Week 1 Day 1

---
+++ Week01_AsyncAwait/Day1_ConfigureAwait/Program.cs
@@ -0,0 +1,22 @@
+﻿// Avoid capturing context in background tasks
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Starting async call...");
+        await MakeApiCallAsync().ConfigureAwait(false);
+        Console.WriteLine("Finished without capturing context.");
+    }
+
+    static async Task MakeApiCallAsync()
+    {
+        using var httpClient = new HttpClient();
+        var result = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+        Console.WriteLine("Fetched data:");
+        Console.WriteLine(result);
+    }
+}
