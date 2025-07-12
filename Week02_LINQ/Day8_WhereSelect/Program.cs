39b865320c547f2e870118499a82ed365472afb2
Avijit Roy <mail@avijitroy.com>
2025-07-12 04:57:26 -0400

    Update Day 8

--- Week02_LINQ/Day8_WhereSelect/Program.cs
+++ Week02_LINQ/Day8_WhereSelect/Program.cs
@@ -1,12 +1,14 @@
 // Summary: Filters students with score > 85 and selects their names
 // Day 8: Where and Select Example
-    // Author: Avijit Roy
+// Author: Avijit Roy
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 class Program
 {
+    // Define a simple Student class with Name and Score properties
     class Student
     {
         public string Name { get; set; }
@@ -15,18 +17,23 @@ class Program
 
     static void Main()
     {
+        // Sample data: a list of students with their scores
         var students = new List<Student>
         {
             new Student { Name = "Avijit", Score = 92 },
             new Student { Name = "Proma", Score = 88 },
-            new Student { Name = "Zahin", Score = 79 },
-            new Student { Name = "Anika", Score = 95 }
+            new Student { Name = "Parinita", Score = 79 },
+            new Student { Name = "Ami", Score = 95 }
         };
 
+        // LINQ chain:
+        // - Where: filters students with Score > 85
+        // - Select: projects only the Name of those students
         var toppers = students
             .Where(s => s.Score > 85)
             .Select(s => s.Name);
 
+        // Output the names of top scorers
         Console.WriteLine("Top Scorers:");
         foreach (var name in toppers)
             Console.WriteLine(name);
